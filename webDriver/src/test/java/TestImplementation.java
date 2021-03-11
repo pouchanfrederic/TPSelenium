d3ecc2b84fdf1703f2423676858b629a397b6097
Tom Avenel <tomavenel@gmail.com>
2021-03-11 16:49:54 +0100

    template

---
+++ webDriver/src/test/java/TestImplementation.java
@@ -0,0 +1,38 @@
+import org.junit.*;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import static org.hamcrest.CoreMatchers.containsString;
+import static org.hamcrest.MatcherAssert.assertThat;
+
+public class TestImplementation {
+
+    private WebDriver driver;
+
+    @BeforeClass
+    public static void setupWebdriverChromeDriver() {
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver");
+        // for Firefox 
+	// System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/src/test/resources/geckodriver");
+    }
+
+    @Before
+    public void setup() {
+        driver = new ChromeDriver();
+        // for Firefox
+	// driver = new FirefoxDriver();
+    }
+
+    @After
+    public void teardown() {
+        if (driver != null) {
+            driver.quit();
+        }
+    }
+
+    @Test
+    public void verifyGitHubTitle() {
+        driver.get("https://www.github.com");
+        assertThat(driver.getTitle(), containsString("GitHub"));
+    }
+}
