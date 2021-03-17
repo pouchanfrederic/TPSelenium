cee3269af06cf278d25ceb584710787a68d1260c
Pouchan Frédéric <57229753+pouchanfrederic@users.noreply.github.com>
2021-03-17 11:21:50 +0100

    First commit with different files

--- webDriver/src/test/java/TestImplementation.java
+++ webDriver/src/test/java/TestImplementation.java
@@ -2,8 +2,16 @@ import org.junit.*;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-import static org.hamcrest.CoreMatchers.containsString;
-import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.MatcherAssert.*;
+
+import org.junit.Test;
+import org.junit.Before;
+import org.junit.After;
+// import static org.junit.Assert.*;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Dimension;
+import org.openqa.selenium.Keys;
 
 public class TestImplementation {
 
@@ -11,7 +19,7 @@ public class TestImplementation {
 
     @BeforeClass
     public static void setupWebdriverChromeDriver() {
-        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver");
+        System.setProperty("webdriver.chrome.driver", System.getProperty("user.dir") + "/src/test/resources/chromedriver.exe");
         // for Firefox 
 	// System.setProperty("webdriver.gecko.driver", System.getProperty("user.dir") + "/src/test/resources/geckodriver");
     }
@@ -34,5 +42,32 @@ public class TestImplementation {
     public void verifyGitHubTitle() {
         driver.get("https://www.github.com");
         assertThat(driver.getTitle(), containsString("GitHub"));
+    }
+
+    @Test
+    public void verifyTestTitle(){
+        driver.get("https://lambdatest.github.io/sample-todo-app/");
+        assertThat(driver.getTitle(), containsString("Sample"));
+    }
+
+
+    /**
+     * Test qui ajoute une nouvelle checkbox, puis test si elle existe bien une fois qu'elle est suposée avoir été créée 
+     */
+
+    @Test
+    public void verifySixthItem(){
+
+        driver.get("https://lambdatest.github.io/sample-todo-app/");
+        // 2 | setWindowSize | 1874x1096 |  |
+        driver.manage().window().setSize(new Dimension(1874, 1096));
+        // 3 | click | id=sampletodotext |  |
+        driver.findElement(By.id("sampletodotext")).click();
+        // 4 | type | id=sampletodotext | Sixth Item |
+        driver.findElement(By.id("sampletodotext")).sendKeys("Sixth Item");
+        // 5 | sendKeys | id=sampletodotext | ${KEY_ENTER} |
+        driver.findElement(By.id("sampletodotext")).sendKeys(Keys.ENTER);
+
+        assertThat(driver.findElement(By.name("li6")) , notNullValue());
     }
 }
